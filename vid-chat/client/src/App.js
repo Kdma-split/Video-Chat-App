78c8f37f8436201f81d9c9ac46b28038b330907c
Kdma-split <amdwebdev69@gmail.com>
2025-04-09 19:01:55 +0530

    Update App.js

--- vid-chat/client/src/App.js
+++ vid-chat/client/src/App.js
@@ -1,51 +1,12 @@
-import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Routes, Route } from 'react-router-dom';
+import VideoChat from './pages/VideoChat.page.jsx';
 
-import VideoPlayer from './components/VideoPlayer';
-import Sidebar from './components/Sidebar';
-import Notifications from './components/Notifications';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: '30px 100px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '600px',
-    border: '2px solid black',
-
-    [theme.breakpoints.down('xs')]: {
-      width: '90%',
-    },
-  },
-  image: {
-    marginLeft: '15px',
-  },
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%',
-  },
-}));
-
-const App = () => {
-  const classes = useStyles();
-
+function App() {
   return (
-    <div className={classes.wrapper}>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
-      </AppBar>
-      <VideoPlayer />
-      <Sidebar>
-        <Notifications />
-      </Sidebar>
-    </div>
-  );
-};
+    <Routes>
+      <Route path="/video-chat" element={ <VideoChat /> }/>
+    </Routes>
+  )
+}
 
 export default App;
